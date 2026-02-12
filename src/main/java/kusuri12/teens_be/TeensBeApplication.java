d77762c168f07cdf98c4ba5db0221337f8f28a20
kusuri12 <waterfail12@naver.com>
2026-02-12 11:42:57 +0900

    feat: ban 어노테이션 추가

--- src/main/java/kusuri12/teens_be/TeensBeApplication.java
+++ src/main/java/kusuri12/teens_be/TeensBeApplication.java
@@ -1,13 +1,13 @@
 package kusuri12.teens_be;
 
 import kusuri12.teens_be.global.jwt.JwtProperties;
-import kusuri12.teens_be.global.s3.S3Properties;
+import kusuri12.teens_be.global.aws.AwsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({JwtProperties.class, S3Properties.class})
+@EnableConfigurationProperties({JwtProperties.class, AwsProperties.class})
 public class TeensBeApplication {
 
     public static void main(String[] args) {
