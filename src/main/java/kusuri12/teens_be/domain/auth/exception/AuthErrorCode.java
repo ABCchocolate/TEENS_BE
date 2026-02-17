31df65b39d53883b083ef6086e70a93f9286aec6
kusuri12 <waterfail12@naver.com>
2026-02-17 22:30:17 +0900

    fix: 잘못 매핑된 에러 코드 수정

--- src/main/java/kusuri12/teens_be/domain/auth/exception/AuthErrorCode.java
+++ src/main/java/kusuri12/teens_be/domain/auth/exception/AuthErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum AuthErrorCode implements ErrorCode {
 
     // 400
-    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "AUT_400_01", "아이디 또는 비밀번호가 일치하지 않습니다."),
+    INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "AUT_400_01", "아이디 또는 비밀번호가 일치하지 않습니다."),
     PASSWORD_CONFIRM_WRONG(HttpStatus.BAD_REQUEST, "AUT_400_02", "비밀번호가 일치하지 않습니다."),
     INVALID_TOKEN_PAIR(HttpStatus.BAD_REQUEST, "AUT_400_03", "Access Token과 Refresh Token의 정보가 일치하지 않습니다."),
     EMAIL_CODE_MISMATCH(HttpStatus.BAD_REQUEST, "AUT_400_04", "인증 코드가 일치하지 않습니다. 다시 확인해주세요."),
