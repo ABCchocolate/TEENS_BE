556aec48703ef234b71baeabaf3d5dceb7a8ec26
kusuri12 <waterfail12@naver.com>
2026-02-20 09:48:52 +0900

    chore: 사용되지 않는 import 제거

--- src/main/java/kusuri12/teens_be/global/config/JacksonConfig.java
+++ src/main/java/kusuri12/teens_be/global/config/JacksonConfig.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
