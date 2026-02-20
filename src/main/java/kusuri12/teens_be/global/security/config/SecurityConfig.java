1866308c3c4d85ffe457e9b015dfff029df60e94
kusuri12 <waterfail12@naver.com>
2026-02-20 09:49:23 +0900

    chore: profile 오타 수정

--- src/main/java/kusuri12/teens_be/global/security/config/SecurityConfig.java
+++ src/main/java/kusuri12/teens_be/global/security/config/SecurityConfig.java
@@ -26,7 +26,7 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@Profile("prob")
+@Profile("prod")
 public class SecurityConfig {
 
     private final JwtTokenFilter jwtTokenFilter;
